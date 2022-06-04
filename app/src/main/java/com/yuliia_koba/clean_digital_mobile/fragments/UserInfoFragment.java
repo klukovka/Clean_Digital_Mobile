4f20416384c8fd959d82d00c5aee9c1b0537b5cc
Yuliia Koba <yuliia.koba@proarea.com>
2022-06-04 15:18:56 +0300

    UserInfoFragment

--- app/src/main/java/com/yuliia_koba/clean_digital_mobile/fragments/UserInfoFragment.java
+++ app/src/main/java/com/yuliia_koba/clean_digital_mobile/fragments/UserInfoFragment.java
@@ -3,64 +3,92 @@ package com.yuliia_koba.clean_digital_mobile.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yuliia_koba.clean_digital_mobile.R;
+import com.yuliia_koba.clean_digital_mobile.models.Status;
+import com.yuliia_koba.clean_digital_mobile.models.dto.Client;
+import com.yuliia_koba.clean_digital_mobile.view_models.EventsViewModel;
+import com.yuliia_koba.clean_digital_mobile.view_models.UserInfoViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UserInfoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class UserInfoFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private UserInfoViewModel viewModel;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public UserInfoFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UserInfoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UserInfoFragment newInstance(String param1, String param2) {
-        UserInfoFragment fragment = new UserInfoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    TextView name, email, phone, bonuses, sale;
+    ProgressBar progressBar;
+    Button editUser, editPassword;
+    LinearLayout layout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        viewModel = new ViewModelProvider(this).get(UserInfoViewModel.class);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_user_info, container, false);
+
+        name = view.findViewById(R.id.client_name);
+        email = view.findViewById(R.id.client_email);
+        phone = view.findViewById(R.id.client_phone);
+        bonuses = view.findViewById(R.id.client_bonuses);
+        sale = view.findViewById(R.id.client_sale);
+        progressBar = view.findViewById(R.id.info_progressBar);
+        editUser = view.findViewById(R.id.edit_info);
+        editPassword = view.findViewById(R.id.edit_password);
+        layout = view.findViewById(R.id.user_info_layout);
+
+
+        viewModel.getStatus().observe(getActivity(), status -> {
+            progressBar.setVisibility(Status.LOADING == status ? View.VISIBLE : View.GONE);
+            layout.setVisibility(Status.LOADING == status ? View.GONE : View.VISIBLE);
+        });
+
+        viewModel.getErrorMessage().observe(getActivity(), s ->
+                Toast.makeText(getActivity(), s, Toast.LENGTH_SHORT).show());
+
+        viewModel.getClient().observe(getActivity(), client -> {
+            name.setText(client.name);
+            email.setText(client.user.getEmail());
+            phone.setText(client.phone);
+            sale.setText(String.valueOf(client.sale));
+            bonuses.setText(String.valueOf(client.bonuses));
+        });
+
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        editPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        editUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
